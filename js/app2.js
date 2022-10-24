c3eafc39d12a88b8977f9c0d4a4e73576a5edd0c
Aurel Cozmulici <aurel.cozmulici@dedagroup.it>
2022-10-24 14:28:45 +0200

    Visualizziamo l'intero oggetto in un elenco

--- js/app2.js
+++ js/app2.js
@@ -3,5 +3,10 @@ Vue.createApp({
         title: 'Titolo',
         myHTML: '<h2>Testo html scritto in JS</h2>',
         titleH3: 'Elenco',
+        person: {
+            firstName: "Aurel",
+            lastName: "Cozmulici",
+            age: 29
+        }
     }),
 }).mount('#app');
