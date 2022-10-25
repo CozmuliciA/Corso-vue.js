01532e6e91194405365eafb9eb10ffc418b5c03c
Aurel Cozmulici <aurel.cozmulici@dedagroup.it>
2022-10-25 14:13:12 +0200

    Aggiungiamo/diminuiamo l'età con 2 bottoni

--- js/app3.js
+++ js/app3.js
@@ -2,7 +2,13 @@ const app = Vue.createApp({
     //template: '<h1>I am the template (js)</h1>'
     data: () => ({
         h2: 'I am H2 (js)',
-    })
+        person: {
+            firstName: "Aurel",
+            lastName: "Cozmulici",
+            age: 29
+        }
+    }),
+    methods: {},
 })
 
 app.mount('#app')
